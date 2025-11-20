6de57b3f3f45c3a5828da5ce18aa1214064f5d3b
Angel Rodrigo <arbyking102@gmail.com>
2025-11-20 12:07:40 -0600

    Corrección link

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configura tu webhook aquí
-const WEBHOOK_URL = "https://arbyk.app.n8n.cloud/webhook-test/operacion";
+const WEBHOOK_URL = "https://arbyk.app.n8n.cloud/webhook/operacion";
 
 document.addEventListener('DOMContentLoaded', () => {
   // Soporta #mathForm (tu HTML actual) y fallback a #opForm si lo usas en otra vista
